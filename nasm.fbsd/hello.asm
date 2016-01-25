896e25f217dceec07402abdffa10c1aa4c6b6e7b
Qi Wenmin <qiwenmin@gmail.com>
2016-01-25 18:36:14 +0800

    添加FreeBSD上的NASM汇编hello程序
    目前是32位版本。

---
+++ nasm.fbsd/hello.asm
@@ -0,0 +1,16 @@
+	%include	'system.inc'
+
+	section	.data
+	hello	db	'Hello, world!', 0Ah
+	hbytes	equ	$-hello
+
+	section	.text
+	global	_start
+_start:
+	push	dword hbytes
+	push	dword hello
+	push	dword stdout
+	sys.write
+
+	push	dword 0
+	sys.exit
